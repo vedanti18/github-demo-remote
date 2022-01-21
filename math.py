e45b0058ac102032cdef2f1f485a699490d82032
vedanti18 <68323255+vedanti18@users.noreply.github.com>
2022-01-21 19:53:54 +0530

    Update math.py

--- math.py
+++ math.py
@@ -1,18 +1,11 @@
-#Add Implementation
 def add(x,y):
-    return x+y
-#Subtract Implementation
-def subtract(x,y):
-    return x-y
-#Multiply Implementation
-def multiply(x,y):
-    return x*y
-#Divide Implementation
-def divide(x,y):
-       if y==0:
-          return DIVIDE_BY_0_ERROR
-       else:
-          return x/y
+    pass
 
-def square(x):
-     return x*x
+def subtract(x,y):
+    pass
+
+def multiply(x,y):
+    pass
+
+def divide(x,y):
+    pass
