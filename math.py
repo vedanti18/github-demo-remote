3aaa071c6a780cbf1dd1f811894af1a0c2d6cf15
vedanti18 <68323255+vedanti18@users.noreply.github.com>
2022-01-21 19:48:47 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,18 @@
+#Add Implementation
+def add(x,y):
+    return x+y
+#Subtract Implementation
+def subtract(x,y):
+    return x-y
+#Multiply Implementation
+def multiply(x,y):
+    return x*y
+#Divide Implementation
+def divide(x,y):
+       if y==0:
+          return DIVIDE_BY_0_ERROR
+       else:
+          return x/y
+
+def square(x):
+     return x*x
